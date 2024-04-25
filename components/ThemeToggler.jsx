2132f6904103b1072a186e12e2766058a01c0106
talarys <d.simon.ssx0@gmail.com>
2024-04-25 19:49:49 +0200

    Added themetoggle

---
+++ components/ThemeToggler.jsx
@@ -0,0 +1,19 @@
+"use client"
+
+import { Button } from "./ui/button"
+import { MoonIcon, SunIcon } from "@radix-ui/react-icons"
+import { useTheme } from "next-themes"
+
+const ThemeToggler = () => {
+	const { theme, setTheme } = useTheme()
+	return (
+		<div>
+			<Button onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
+				<SunIcon className="h-5 w-5 transition-all dark:scale-0 scale-100 rotate-0 dark:-rotate-90" />
+				<MoonIcon className="h-5 w-5 absolute transition-all scale-0 dark:scale-100 rotate-90 dark:rotate-0 " />
+			</Button>
+		</div>
+	)
+}
+
+export default ThemeToggler
