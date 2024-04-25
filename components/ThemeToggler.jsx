40e57ba8cf9c964aa076ebb779f2888266b9b1cf
talarys <d.simon.ssx0@gmail.com>
2024-04-25 20:15:07 +0200

    Rounded

--- components/ThemeToggler.jsx
+++ components/ThemeToggler.jsx
@@ -8,8 +8,11 @@ const ThemeToggler = () => {
 	const { theme, setTheme } = useTheme()
 	return (
 		<div>
-			<Button onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
-				<SunIcon className="h-5 w-5 transition-all dark:scale-0 scale-100 rotate-0 dark:-rotate-90" />
+			<Button
+				className="rounded-full p-5"
+				onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+			>
+				<SunIcon className="h-5 w-5 absolute transition-all dark:scale-0 scale-100 rotate-0 dark:-rotate-90" />
 				<MoonIcon className="h-5 w-5 absolute transition-all scale-0 dark:scale-100 rotate-90 dark:rotate-0 " />
 			</Button>
 		</div>
