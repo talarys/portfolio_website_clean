14e94c611d4202db9cb2d665da5bfa107207df18
talarys <d.simon.ssx0@gmail.com>
2024-04-29 22:18:09 +0200

    Finished MobileNav

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -1,5 +1,29 @@
+import { Sheet, SheetTrigger, SheetContent } from "./ui/sheet"
+import { LuAlignJustify } from "react-icons/lu"
+import Nav from "./Nav"
+import Logo from "./Logo"
+import Socials from "./Socials"
+
 const MobileNav = () => {
-	return <div>MobileNav</div>
+	return (
+		<Sheet>
+			<SheetTrigger>
+				<LuAlignJustify />
+			</SheetTrigger>
+			<SheetContent>
+				<div className="flex flex-col items-center justify-between h-full py-8">
+					<div className="flex flex-col gap-y-32 items-center">
+						<Logo />
+						<Nav
+							containerStyles={"flex flex-col items-center gap-y-6"}
+							linkStyles={"text-2xl"}
+						/>
+					</div>
+					<Socials containerStyles={"flex gap-x-4"} iconsStyles={"text-2xl"} />
+				</div>
+			</SheetContent>
+		</Sheet>
+	)
 }
 
 export default MobileNav
