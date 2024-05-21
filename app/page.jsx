8f8f583f1cc8ecfdc035973469997de8190579f2
talarys <d.simon.ssx0@gmail.com>
2024-05-21 21:53:59 +0200

    Forgot to remove unused css

--- app/page.jsx
+++ app/page.jsx
@@ -5,7 +5,7 @@ import Services from "@/components/Services"
 
 export default function Home() {
 	return (
-		<main className="snap-mandatory snap-y overflow-y-scroll h-screen">
+		<main>
 			<Hero />
 			<About />
 			<Services />
